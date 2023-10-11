88fd70f94e7046b972f867323015eccd0fbde047
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-11 21:14:24 +0000

    doctype

--- dice.rb
+++ dice.rb
@@ -36,17 +36,17 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-
+erb(:two_ten)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
 
-  outcome = "You rolled a #{die}."
+  @outcome = "You rolled a #{die}."
 
-
+erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -57,12 +57,14 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum =  first_die + second_die + third_die + fourth_die + fifth_die
 
-outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
 
+erb(:five_four)
 end
 
 get("/dice/1/6") do
   die = rand(1..6)
-  outcome = "Your rolled a #{die}"
+ @outcome = "Your rolled a #{die}."
 
+erb(:one_six)
 end
